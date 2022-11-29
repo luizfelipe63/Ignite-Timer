7564c6107d20e80599b16bbe4bb20846202ec197
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2022-11-29 14:52:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/button"
+
+
+
+export function App() {
+
+  return (
+    <>
+    <Button variant="primary" />
+    <Button variant="secundary" />
+    <Button variant="danger" />
+    <Button variant="success" />
+    <Button/>
+    </>
+
+  )
+}
+
+
---
+++ src/components/button.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer, ButtonVariant } from "./button.styles"
+
+interface buttonProps {
+  variant?: ButtonVariant
+}
+
+export function Button({variant = 'primary'}:buttonProps ){
+  return( 
+    <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+  )
+}
